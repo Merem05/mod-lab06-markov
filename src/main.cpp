d2200098f7e10903ee323a70f29b498d29efab6b
Merem05 <mashaeremenko2005@gmail.com>
2026-05-02 12:29:13 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,18 @@
+// Copyright 2024
+
+#include <iostream>
+#include <string>
 #include "textgen.h"
 
 int main() {
+    std::cout << "=== MARKOV TEXT GENERATOR ===\n";
     std::cout << "Prefix size: " << NPREF << " words\n";
     std::cout << "Max text size: " << MAXGEN << " words\n\n";
 
     statetab table;
     prefix startPrefix;
 
-    string filename = "input.txt";
+    std::string filename = "input.txt";
     std::cout << "Loading file: " << filename << "...\n";
 
     buildTable(filename, table, startPrefix);
@@ -19,7 +24,7 @@ int main() {
 
     std::cout << "File loaded successfully\n";
 
-    string generatedText = generate(table, startPrefix, MAXGEN);
+    std::string generatedText = generate(table, startPrefix, MAXGEN);
 
     if (generatedText.empty()) {
         std::cerr << "Error: cannot generate text\n";
